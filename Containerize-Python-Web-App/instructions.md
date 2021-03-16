8ea7b19d1370225cfe18e6902e382402c488b3c0
Michael Levan <mlevan1992@gmail.com>
2021-03-16 09:52:33 -0400

    Python web API

---
+++ Containerize-Python-Web-App/instructions.md
@@ -0,0 +1,5 @@
+1. Build the Docker image:
+  `docker build -t pyweb .`
+
+2. Run the Docker image
+  `docker run -p 5000:5000 -tid pyweb`
---
+++ Containerize-Python-Web-App/pyweb/pyweb.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, jsonify
+
+
+pyweb = Flask(__name__)
+
+
+@pyweb.route("/")
+def home():
+    return 'Hello, YouTube Friends!'
+
+
+pyweb.run(host='0.0.0.0', port=5000)
