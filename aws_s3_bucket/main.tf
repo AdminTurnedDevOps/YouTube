406a38182a4e3aa74a7e39969a2e8060dfb2f728
Michael Levan <mlevan1992@gmail.com>
2021-12-08 08:47:11 -0500

    s3

---
+++ aws_s3_bucket/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-the-hard-way"
+    key    = "srecourse.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "srecoursebuf_s3" {
+  bucket = "srecoursebuf_s3_bucket"
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
--- azure-resource-group/az_sp_create.ps1
+++
@@ -1,9 +0,0 @@
-function create_sp {
-    [cmdletbinding(ConfirmImpact='low')]
-    param(
-        [string]$appName = 'githubactionssrecourse',
-        [string]$subID = ''
-    )
-    
-    az ad sp create-for-rbac -n $appName --role Contributor --scopes /subscriptions/$subID
-}
